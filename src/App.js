ecab4decd36b160949eb04cd639127dd423bb072
sung-mi <pinkjoau@gmail.com>
2023-10-25 00:34:48 +0900

    redux추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import styled from 'styled-components';
 import Loca from './components/Loca'
 import Card from './components/Card'
 import Tap from './components/Tap'
+import miseData from './api/data.json'
+
+
+console.log(miseData);
+console.log(miseData.response.body.items[0].sidoName)
+// const misedata = JSON.stringify(miseData)
+// console.log(misedata)
 
 function App() {
   return (
@@ -12,6 +19,8 @@ function App() {
     </Container>
   );
 }
+
+
 
 export default App;
 
